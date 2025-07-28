4b7320774002088dc49e6f0993dd95a64d2c2bc8
figueiredomarcelo <137330786+figueiredomarcelo@users.noreply.github.com>
2025-07-28 13:02:58 -0300

    Add files via upload

--- banner.js
+++ banner.js
@@ -1,4 +1,4 @@
-const url = "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd";
+const url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
 
 let player;
 
