9743462c8e418a68c636c6a15e9a24fc0a22aacb
figueiredomarcelo <137330786+figueiredomarcelo@users.noreply.github.com>
2025-07-28 15:53:14 -0300

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,58 @@
+const url = "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd";
+
+let player;
+
+window.addEventListener("load", async () => {
+  try {
+    await senza.init();
+    player = new senza.ShakaPlayer();
+    player.configure(playerConfig());
+    player.attach(video);
+    await player.load(url);
+    await video.play();
+
+    senza.lifecycle.autoBackground = true;
+    senza.lifecycle.autoBackgroundDelay = 15;
+
+    senza.uiReady();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+document.addEventListener("keydown", async function (event) {
+  switch (event.key) {
+    case "Enter": await toggleBackground(); break;
+    case "Escape": await playPause(); break;
+    case "ArrowUp": break;
+    case "ArrowDown": break;
+    case "ArrowLeft": skip(-30); break;
+    case "ArrowRight": skip(30); break;
+    default: return;
+  }
+  event.preventDefault();
+});
+
+async function toggleBackground() {
+  if (senza.lifecycle.state == senza.lifecycle.UiState.BACKGROUND) {
+    await senza.lifecycle.moveToForeground();
+  } else {
+    await senza.lifecycle.moveToBackground();
+  }
+}
+
+async function playPause() {
+  if (video.paused) {
+    await video.play();
+  } else {
+    await video.pause();
+  }
+}
+
+function skip(seconds) {
+  video.currentTime = video.currentTime + seconds;
+}
+
+function playerConfig() {
+  return {abr: {restrictions: {maxHeight: 1080}}};
+}
